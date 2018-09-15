faeafcdca29a2ac371f7f0a88d96d18ffb412b17
theresamorelli <theresa.l.morelli@gmail.com>
2018-09-15 12:27:37 -0400

    add welcome controller and view, add bootstrap styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'welcome/home'
   resources :offers
   resources :users
+  root 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
