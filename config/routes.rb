ed52f8272fcc00d093456fee21da88b9974eb576
theresamorelli <theresa.l.morelli@gmail.com>
2018-09-15 13:14:36 -0400

    add sessions controller, login view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get 'sessions/destroy'
   get 'welcome/home'
   resources :offers
   resources :users
