7464fc3f2fa7e2764f866f8b29d2b3f318ba6975
theresamorelli <theresa.l.morelli@gmail.com>
2018-09-15 20:57:21 -0400

    add new/create/show offers

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,14 +1,23 @@
 class OffersController < ApplicationController
+  def create
+    offer = current_user.giver.offers.build(offer_params)
+    if offer.save
+      redirect_to offer_path(offer)
+    else
+      flash[:error] = user.errors.full_messages
+      render :new
+    end
+  end
+
   def index
   end
     
   def show
+    @offer = Offer.find_by(id: params[:id])
   end
 
   def new
-  end
-
-  def create
+    @offer = Offer.new
   end
 
   def edit
@@ -18,5 +27,11 @@ class OffersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:title, :description, :availability, :expiration, :fulfilled)
   end
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,4 +2,6 @@ class Offer < ApplicationRecord
   belongs_to :giver
   has_many :offers_receivers
   has_many :receivers, through: :offers_receivers
+
+  validates :title, presence: true
 end
