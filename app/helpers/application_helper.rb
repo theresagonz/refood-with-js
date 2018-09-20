d203a195c125aa2a7c9b464f56172a52bf5775d3
theresamorelli <theresa.l.morelli@gmail.com>
2018-09-20 16:15:40 -0400

    add time_ago helper, add styling to offers lists

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def time_string(unit_count)
+    unit_count.floor.to_s
+  end
+
+  def time_ago(resource)
+    seconds_ago = Time.now - resource.created_at
+    minutes_ago = seconds_ago/60
+    hours_ago = minutes_ago/60
+    days_ago = hours_ago/24
+
+    if days_ago.floor > 0
+      time_string(days_ago) + " days"
+    elsif hours_ago.floor > 0
+      time_string(hours_ago) + " hours"
+    elsif minutes_ago.floor > 0
+      time_string(minutes_ago) + " minutes"
+    else
+      seconds_ago.floor + " seconds"
+    end
+  end
 end
