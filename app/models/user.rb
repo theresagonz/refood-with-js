d33135cfb0380f2f141112aefc5c40515b8f60d5
theresamorelli <theresa.l.morelli@gmail.com>
2018-10-01 12:37:21 -0400

    add user instance method 'karma', change 'point' references to 'karma', add green-text css class

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,8 @@ class User < ApplicationRecord
     message: "only allows numbers" }
   validates :password, presence: true, allow_nil: true
   validates :password_confirmation, presence: true, allow_nil: true
+
+  def karma
+    self.giver.requests.select { |r| r.completed_giver }.count + self.requestor.requests.select { |r| r.completed_requestor }.count
+  end
 end
