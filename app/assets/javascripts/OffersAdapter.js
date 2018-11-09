3d36490dc20dc7086c15b2c420760602b5b7cdff
theresamorelli <theresa.l.morelli@gmail.com>
2018-11-08 19:11:18 -0500

    update baseURL variable, add getOffer method to adapter, remove erb extension from js filenames

--- app/assets/javascripts/OffersAdapter.js
+++ app/assets/javascripts/OffersAdapter.js
@@ -1,13 +1,18 @@
 class OffersAdapter {
   // provides interaction with API
   constructor() {
-    this.baseUrl = 'http://localhost:3000/offers.json';
+    this.baseUrl = 'http://localhost:3000';
   }
 
   getOffers() {
-    return fetch(this.baseUrl).then(res => {
+    return fetch(`${this.baseUrl}/offers.json`).then(res => {
       return res.json();
     }).catch(error => console.error(error));
+  }
 
+  getOffer(id) {
+    return fetch(`${this.baseUrl}/offer/${id}.json`).then(res => {
+      return res.json();
+    }).catch(error => console.error(error));
   }
 }
