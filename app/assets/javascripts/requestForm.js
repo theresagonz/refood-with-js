eb5d839986560f7893baf57b3c980fb117c4e823
theresamorelli <theresa.l.morelli@gmail.com>
2018-11-11 14:41:33 -0500

    Add JSON object to capture form data

--- app/assets/javascripts/requestForm.js
+++ app/assets/javascripts/requestForm.js
@@ -2,6 +2,14 @@ $(() => {
   $('#request-form-wrapper').on('submit', '#request-form', (e) => {
   alert('Hi!');
   e.preventDefault();
-  console.log(this)
+
+  const data = {
+    request: {
+      message: $('#request-message')[0].value,
+      requestor_email: $('#requestor-email')[0].value,
+      requestor_phone: $('#requestor-phone')[0].value
+    }
+  };
+  console.log(data);
   });
 });
